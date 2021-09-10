c7d3935fe8d6e589f067fbe7c8bae21ff1b42c08
Dohyun Ahn <dohyun10101@gmail.com>
2021-09-10 16:12:45 +0900

    Update index.js

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,7 +1,6 @@
 import Layout from 'components/_base/Layout'
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { loadUser, signOut } from 'store/modules/user'
 
 function LayoutContainer({ children }) {
